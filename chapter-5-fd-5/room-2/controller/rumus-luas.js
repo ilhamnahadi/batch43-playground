155657c46873cddce26fa6832d1b7c1a28e2fe55
Adissosis <109132874+Adissosis@users.noreply.github.com>
2024-03-14 15:27:25 +0000

    rework

--- chapter-5-fd-5/room-2/controller/rumus-luas.js
+++ chapter-5-fd-5/room-2/controller/rumus-luas.js
@@ -4,6 +4,6 @@ function hitungLuas(dariClient, untukClient){
     const tinggi = data.tinggi
     const luas = alas * tinggi / 2 
     untukClient.status(200).send('Luas:' + luas)
-}
+};
 
 module.exports = hitungLuas;
