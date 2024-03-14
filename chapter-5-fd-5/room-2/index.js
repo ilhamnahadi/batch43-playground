dae96612b0850428363b02ccba43e27528113031
Ahmad Faiz <65196236+DonDfaiz@users.noreply.github.com>
2024-03-14 15:27:06 +0000

    tes

--- chapter-5-fd-5/room-2/index.js
+++ chapter-5-fd-5/room-2/index.js
@@ -5,8 +5,8 @@ server.use(express.urlencoded());
 
 // todo : Controller
 const lihat_history = require("./controller/lihat-history");
-const rumusLuas= require('./controller/rumus-luas')
-const rumusKeliling = require('./controller/rumus-keliling')
+const rumusLuas = require("../room-2/controller/rumus-luas")
+const rumusKeliling =require("../room-2/controller/rumus-keliling")
 
 // todo : Routing
 server.get("/lihat-history", lihat_history);
