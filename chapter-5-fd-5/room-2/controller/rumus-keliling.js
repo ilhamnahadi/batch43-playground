2ff0764bdf11aa27b4bb8c02fa6c749231744b63
DonDfaiz <ah.faiz9297@gmail.com>
2024-03-15 00:20:11 +0700

    fix

--- chapter-5-fd-5/room-2/controller/rumus-keliling.js
+++ chapter-5-fd-5/room-2/controller/rumus-keliling.js
@@ -1,9 +1,17 @@
+const history = require("../model/history")
+
 function hitungKeliling(req, res){
     const data = req.body
     const alas = data.alas
     const tinggi = data.tinggi
     const keliling = alas+ 2 * tinggi
     res.status(200).send('Keliling:' + keliling)
+
+    history.push({
+        Alas : alas,
+        Tinggi : tinggi,
+        Keliling: keliling
+    })
 };
 
 module.exports = hitungKeliling;
--- chapter-5-fd-5/room-2/controller/rumus-luas.js
+++ chapter-5-fd-5/room-2/controller/rumus-luas.js
@@ -1,9 +1,17 @@
+const history = require('../model/history');
+
 function hitungLuas(req, res){
     const data = req.body
     const alas = data.alas
     const tinggi = data.tinggi
     const luas = alas * tinggi / 2 
     res.status(200).send('Luas:' + luas)
+
+    history.push({
+        Alas : alas,
+        Tinggi : tinggi,
+        Luas : luas
+    })
 };
 
 module.exports = hitungLuas;
